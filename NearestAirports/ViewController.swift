96b098ea4fc81f0398225f635fb665722ead0759
Ramesh Prajapati <ramesh.prajapathi@asiglobal.co>
2019-12-14 16:15:06 +0530

    Initial Commit

---
+++ NearestAirports/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NearestAirports
+//
+//  Created by ramesh on 14/12/19.
+//  Copyright © 2019 Ramesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
